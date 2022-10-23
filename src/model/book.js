74d5b4ea0d9db5b19334237cb947b6d95b9cd58c
ThangHNT <thanghn2503@gmail.com>
2022-10-23 09:21:43 +0700

    first commit

--- src/model/book.js
+++ src/model/book.js
@@ -6,8 +6,10 @@ const Book = new Schema({
     bookcode: { type: String, default: '', required: true },
     category: { type: String, default: '', required: true },
     title: { type: String, default: '', required: true },
+    category: { type: String, default: '', required: true },
     author: { type: String, default: '', required: true },
-    approved: { type: String },
+    description: {type:String, required: true, default: ''},
+    releaseDate: { type: String , required: true},
 });
 
 module.exports = mongoose.model('Book', Book);
